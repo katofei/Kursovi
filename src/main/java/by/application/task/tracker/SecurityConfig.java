9621186d179ed63d60c100bc2c897f31fa1a3d56
yekr0717 <g14.katya@gmail.com>
2018-02-01 14:59:16 +0300

    tried to add registration by e-mail it don't work

--- src/main/java/by/application/task/tracker/SecurityConfig.java
+++ src/main/java/by/application/task/tracker/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/registration").permitAll()
+                .antMatchers("/registration").hasAuthority("ADMIN")
                 .antMatchers("/confirmation").permitAll()
                 .and().csrf().disable()
                 .formLogin()
