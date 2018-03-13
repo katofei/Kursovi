6d66eda547d0fbdb0c06d220f5c59df8adbedc79
yekr0717 <g14.katya@gmail.com>
2018-03-13 13:23:43 +0300

    changelog modified

--- src/main/java/by/application/task/tracker/Constants.java
+++ src/main/java/by/application/task/tracker/Constants.java
@@ -1,61 +1,63 @@
 package by.application.task.tracker;
 
-public class Constants {
+public interface Constants {
 
     /*      User statuses    */
-    public static final String USER_ACTIVATED = "Activated";
-    public static final String USER_NOT_ACTIVATED = "Not activated";
-    public static final String USER_NOT_ASSIGNED = "Not assigned";
-    public static final String USER_ASSIGNED = "Assigned";
+    String USER_ACTIVATED = "Activated";
+    String USER_NOT_ACTIVATED = "Not activated";
+    String USER_NOT_ASSIGNED = "Not assigned";
+    String USER_ASSIGNED = "Assigned";
 
     /*      User roles    */
-    public static final String USER_ROLE = "USER";
-    public static final String ADMIN_ROLE = "ADMIN";
+    String USER_ROLE = "USER";
+    String ADMIN_ROLE = "ADMIN";
 
     /*      Project roles    */
-    public static final String TEAM_LEAD = "Team lead";
-    public static final String PROJECT_MANAGER = "Project manager";
+    String TEAM_LEAD = "Team lead";
+    String PROJECT_MANAGER = "Project manager";
+    String MANAGER = "Manager";
+    String STREAM_LEAD = "Stream lead";
 
 
     /*      Dashboard statuses    */
-    public static final String ON_HOLD = "On hold";
-    public static final String IN_QA = "In qa";
-    public static final String OPEN = "Open";
-    public static final String IN_BUILD = "In build";
-    public static final String IN_DESIGN = "In design";
-    public static final String IN_ANALYSIS = "In analysis";
-    public static final String REOPENED = "Reopened";
-    public static final String CLOSED = "Closed";
+    String ON_HOLD = "On hold";
+    String IN_QA = "In qa";
+    String OPEN = "Open";
+    String IN_BUILD = "In build";
+    String IN_DESIGN = "In design";
+    String IN_ANALYSIS = "In analysis";
+    String REOPENED = "Reopened";
+    String CLOSED = "Closed";
 
     /*      Tsk statuses    */
-    public static final String IMPLEMENTED = "Implemented";
-    public static final String IN_PROGRESS = "In progress";
-    public static final String NEED_INFO = "Additional info required";
-    public static final String INFO_RECEIVED = "Info received";
-    public static final String READY_FOR_TESTING = "Ready for testing";
+    String IMPLEMENTED = "Implemented";
+    String IN_PROGRESS = "In progress";
+    String NEED_INFO = "Additional info required";
+    String INFO_RECEIVED = "Info received";
+    String READY_FOR_TESTING = "Ready for testing";
 
 
     /*  Notification components*/
 
-    public static final String REGISTRATION_CONFIRM_NOTIFICATION = "Registration Confirmation";
-    public static final String USER_DELETION_NOTIFICATION = "Deletion notification";
+    String REGISTRATION_CONFIRM_NOTIFICATION = "Registration Confirmation";
+    String USER_DELETION_NOTIFICATION = "Deletion notification";
 
-    public static final String USER_ASSIGN_NOTIFICATION = "User assign notification";
+    String USER_ASSIGN_NOTIFICATION = "User assign notification";
 
-    public static final String PROJECT_DEADLINE_NOTIFICATION = "Project deadline notification";
+    String PROJECT_DEADLINE_NOTIFICATION = "Project deadline notification";
 
-    public static final String TASK_ESTIMATION_NOTIFICATION = "Task estimation notification";
-    public static final String TASK_DUEDATE_NOTIFICATION = "Task due date notification";
-    public static final String TASK_CREATION_NOTIFICATION = "New task created";
-    public static final String TASK_MODIFICATION_NOTIFICATION = "Task modification notification";
-
-
-    public static final String DASHBOARD_ESTIMATION_NOTIFICATION = "Dashboard deadline notification";
-    public static final String DASHBOARD_DUEDATE_NOTIFICATION = "Dashboard due date notification";
-    public static final String DASHBOARD_CREATION_NOTIFICATION = "New dashboard created";
-    public static final String DASHBOARD_MODIFICATION_NOTIFICATION = "Dashboard modification notification";
+    String TASK_ESTIMATION_NOTIFICATION = "Task estimation notification";
+    String TASK_DUEDATE_NOTIFICATION = "Task due date notification";
+    String TASK_CREATION_NOTIFICATION = "New task created";
+    String TASK_MODIFICATION_NOTIFICATION = "Task modification notification";
 
 
-    public static final String from_email = "Task Tracker System";
+    String DASHBOARD_ESTIMATION_NOTIFICATION = "Dashboard deadline notification";
+    String DASHBOARD_DUEDATE_NOTIFICATION = "Dashboard due date notification";
+    String DASHBOARD_CREATION_NOTIFICATION = "New dashboard created";
+    String DASHBOARD_MODIFICATION_NOTIFICATION = "Dashboard modification notification";
+
+
+    String from_email = "Task Tracker System";
 
 }
